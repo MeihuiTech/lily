00b6b755e4c2b7059658f51fd4917019804cced3
834953332@qq.com <834953332@qq.com>
2021-05-07 16:18:17 +0800

    定时任务

--- miner/miner-server/src/main/java/com/mei/hui/miner/common/task/AggregationTask.java
+++ miner/miner-server/src/main/java/com/mei/hui/miner/common/task/AggregationTask.java
@@ -14,12 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-
-import java.time.LocalDateTime;
 import java.util.List;
 
-@Configuration      //1.主要用于标记配置类，兼备Component的效果。
-@EnableScheduling   // 2.开启定时任务
+@Configuration
+@EnableScheduling
 public class AggregationTask {
     private static final Logger log = LoggerFactory.getLogger(AggregationTask.class);
 
@@ -35,8 +33,7 @@ public class AggregationTask {
      * 账户按天聚合
      */
     //或直接指定时间间隔，例如：5秒
-    //@Scheduled(fixedRate=5000)
-    @Scheduled(cron = "0 0 0 * * ? *")
+    @Scheduled(cron = "0 0 0 */1 * ?")
     public void dailyAccount() {
         log.info("dailyAccount start...");
         SysMinerInfo sysMinerInfo = new SysMinerInfo();
