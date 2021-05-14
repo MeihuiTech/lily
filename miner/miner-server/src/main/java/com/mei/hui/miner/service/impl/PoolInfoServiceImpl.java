533eac90991779d09f85271e507dbdc9bef6bb41
shang_b@meihuitech.com <shang_b@meihuitech.com>
2021-05-14 15:58:49 +0800

    首页用户矿池信息接口增加是否为管理员判断

--- miner/miner-server/src/main/java/com/mei/hui/miner/service/impl/PoolInfoServiceImpl.java
+++ miner/miner-server/src/main/java/com/mei/hui/miner/service/impl/PoolInfoServiceImpl.java
@@ -3,9 +3,11 @@ package com.mei.hui.miner.service.impl;
 import com.mei.hui.miner.entity.PoolInfo;
 import com.mei.hui.miner.mapper.PoolInfoMapper;
 import com.mei.hui.miner.service.IPoolInfoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class PoolInfoServiceImpl implements IPoolInfoService {
 
@@ -14,6 +16,7 @@ public class PoolInfoServiceImpl implements IPoolInfoService {
 
     @Override
     public PoolInfo selectPoolInfoByUserId(Long userId) {
+        // 判断是否是管理员
         PoolInfo info = poolInfoMapper.selectMinerInfoByUserId(userId);
 
         PoolInfo machineInfo = poolInfoMapper.selectMachineInfoByUserId(userId);
