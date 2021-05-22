1a0d3ac3f9af88abfbe35c994be8a54cdbc3a733
shang_b@meihuitech.com <shang_b@meihuitech.com>
2021-05-22 16:53:44 +0800

    MP-33 修改用户信息

--- miner-pool-common/src/main/java/com/mei/hui/util/SmsServiceNameEnum.java
+++ miner-pool-common/src/main/java/com/mei/hui/util/SmsServiceNameEnum.java
@@ -10,7 +10,8 @@ public enum  SmsServiceNameEnum {
 
     withdraw("提取收益"),
     addReceiveAddress("新增收款地址"),
-    updateReceiveAddress("编辑收款地址");
+    updateReceiveAddress("编辑收款地址"),
+    updateUser("修改用户信息");
 
 
     private String serviceName;
