03f07247b03b2fb685e83a4edff2e5c720e5da74
834953332@qq.com <834953332@qq.com>
2021-06-04 18:02:19 +0800

    币种费率远程调用接口

---
+++ miner/miner-feign-client/src/main/java/com/mei/hui/miner/feign/vo/SaveFeeRateBO.java
@@ -0,0 +1,19 @@
+package com.mei.hui.miner.feign.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@ApiModel
+public class SaveFeeRateBO {
+
+    @ApiModelProperty(value = "用户id",required = true)
+    private Long userId;
+
+    @ApiModelProperty(value = "币种费率集合",required = true)
+    List<CurrencyRateBO> rats = new ArrayList<>();
+}
