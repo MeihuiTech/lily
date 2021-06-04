078344e37f2a4313e71973e635e796cf43c659fa
shangbin@meihuitech.com <shangbin@meihuitech.com>
2021-06-04 16:32:26 +0800

    币种枚举类

--- miner/miner-server/src/main/java/com/mei/hui/miner/common/enums/CurrencyEnum.java
+++ miner/miner-server/src/main/java/com/mei/hui/miner/common/enums/CurrencyEnum.java
@@ -5,15 +5,18 @@ package com.mei.hui.miner.common.enums;
  */
 public enum CurrencyEnum {
 
-    FIL(1L,"fil币"),
-    CHIA(2L,"起亚币");
+    FIL(1L,"fil币","FIL"),
+    CHIA(2L,"起亚币","XCH");
 
     private Long currencyId;
     private String des;
+    /*币种代币名称*/
+    private String name;
 
-    CurrencyEnum(Long currencyId,String des){
+    CurrencyEnum(Long currencyId,String des,String name){
         this.currencyId = currencyId;
         this.des = des;
+        this.name = name;
     }
 
     /**
@@ -30,6 +33,33 @@ public enum CurrencyEnum {
         return null;
     }
 
+    /**
+     * 根据币种区块链项目名称type返回币种代币名称name
+     * @param type 别的表里的type：货币种类,FIL,CHIA
+     * @return
+     */
+    public static String getCurrencyNameByProjectName(String type){
+        for(CurrencyEnum currency : CurrencyEnum.values()){
+            if(currency.name().equals(type)){
+                return currency.getName().toString();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 根据币种代币名称name查出来币种区块链项目名称type
+     * @param projectName
+     * @return
+     */
+    public static String getProjectNameByCurrencyName(String currencyName){
+        for(CurrencyEnum currency : CurrencyEnum.values()){
+            if(currency.getName().equals(currencyName)){
+                return currency.name().toString();
+            }
+        }
+        return null;
+    }
 
     public Long getCurrencyId() {
         return currencyId;
@@ -47,4 +77,11 @@ public enum CurrencyEnum {
         this.des = des;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
