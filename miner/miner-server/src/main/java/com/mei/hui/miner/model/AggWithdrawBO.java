4e6029ba05f0779c7523e14f362636ee335798c4
shangbin@meihuitech.com <shangbin@meihuitech.com>
2021-06-05 10:34:17 +0800

    MP-210 后端：管理员-矿池收益-用户提取记录增加查询条件：币种、状态、用户名

--- miner/miner-server/src/main/java/com/mei/hui/miner/model/AggWithdrawBO.java
+++ miner/miner-server/src/main/java/com/mei/hui/miner/model/AggWithdrawBO.java
@@ -7,7 +7,7 @@ import lombok.Builder;
 import lombok.Data;
 
 @Data
-@ApiModel("管理员-矿池收益-用户提币汇总分页入参")
+@ApiModel("管理员-矿池收益-用户提币分页入参")
 public class AggWithdrawBO extends BasePage {
 
     @ApiModelProperty("用户名称")
@@ -15,5 +15,8 @@ public class AggWithdrawBO extends BasePage {
 
     @ApiModelProperty(value = "币种id")
     private Long currencyId;
+
+    @ApiModelProperty(value = "0 提币中 1 提币完成 2 提币失败")
+    private Integer status;
 
 }
