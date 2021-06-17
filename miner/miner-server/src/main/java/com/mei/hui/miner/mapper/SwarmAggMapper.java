d984e8723aa83f282b7d221ddf98bf846bf26fbb
834953332@qq.com <834953332@qq.com>
2021-06-17 16:52:20 +0800

    MP-298 swarm 节点分页列表

--- miner/miner-server/src/main/java/com/mei/hui/miner/mapper/SwarmAggMapper.java
+++ miner/miner-server/src/main/java/com/mei/hui/miner/mapper/SwarmAggMapper.java
@@ -1,14 +1,23 @@
 package com.mei.hui.miner.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.mei.hui.miner.entity.PerTicket;
 import com.mei.hui.miner.entity.SwarmAgg;
+import org.apache.ibatis.annotations.Param;
+import org.apache.poi.ss.formula.functions.T;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Map;
 
 @Repository
 public interface SwarmAggMapper extends BaseMapper<SwarmAgg> {
 
-    List<Map<String,Object>> getPerTicketInfo(Map<String,Object> map);
+    List<PerTicket> getPerTicketInfo(Map<String,Object> map);
+
+    IPage<PerTicket> perTicketPageList(Page page,boolean isAsc);
 }
--- miner/miner-server/src/main/java/com/mei/hui/miner/service/impl/SwarmNodeServiceImpl.java
+++ miner/miner-server/src/main/java/com/mei/hui/miner/service/impl/SwarmNodeServiceImpl.java
@@ -2,30 +2,29 @@ package com.mei.hui.miner.service.impl;
 
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.mei.hui.config.HttpRequestUtil;
 import com.mei.hui.miner.common.MinerError;
+import com.mei.hui.miner.entity.PerTicket;
 import com.mei.hui.miner.entity.SwarmNode;
 import com.mei.hui.miner.feign.vo.NodePageListBO;
 import com.mei.hui.miner.feign.vo.NodePageListVO;
 import com.mei.hui.miner.mapper.SwarmAggMapper;
 import com.mei.hui.miner.mapper.SwarmNodeMapper;
-import com.mei.hui.miner.service.ISwarmAggService;
 import com.mei.hui.miner.service.ISwarmNodeService;
 import com.mei.hui.util.CurrencyEnum;
 import com.mei.hui.util.MyException;
 import com.mei.hui.util.PageResult;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.ss.formula.functions.T;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -49,7 +48,7 @@ public class SwarmNodeServiceImpl extends ServiceImpl<SwarmNodeMapper, SwarmNode
         }
         LambdaQueryWrapper<SwarmNode> query = new LambdaQueryWrapper();
         if(StringUtils.isNotEmpty(bo.getIp())){
-            query.eq(SwarmNode::getId,bo.getIp());
+            query.eq(SwarmNode::getNodeIp,bo.getIp());
         }
         if(bo.getState() != null){
             query.eq(SwarmNode::getState,bo.getState());
@@ -68,8 +67,17 @@ public class SwarmNodeServiceImpl extends ServiceImpl<SwarmNodeMapper, SwarmNode
                 query.orderByDesc(SwarmNode::getLinkNum);
             }
         }
+        //先在聚合表统计节点和昨日出票数，然后进行排序后返回
+        if("yestodayTicketValid".equalsIgnoreCase(bo.getCloumName())){
+            List<String> adrresses = perTicketPageList(bo);
+            if(adrresses != null && adrresses.size() > 0){
+                query.in(SwarmNode::getWalletAddress,adrresses);
+            }
+        }
         query.orderByDesc(SwarmNode::getCreateTime);
+        log.info("查询节点列表，入参:{}",query.getCustomSqlSegment());
         IPage<SwarmNode> page = this.page(new Page<>(bo.getPageNum(), bo.getPageSize()), query);
+        log.info("查询节点列表，出参:{}",JSON.toJSONString(page.getRecords()));
         List<NodePageListVO> list = page.getRecords().stream().map(v -> {
             NodePageListVO vo = new NodePageListVO();
             BeanUtils.copyProperties(v, vo);
@@ -83,6 +91,20 @@ public class SwarmNodeServiceImpl extends ServiceImpl<SwarmNodeMapper, SwarmNode
     }
 
     /**
+     * 按昨日出票数排序后返回节点地址，即节点的唯一标识
+     * @param bo
+     * @return
+     */
+    public List<String> perTicketPageList(NodePageListBO bo){
+        log.info("按昨日出票进行排序,入参:isAsc={}",bo.isAsc());
+        IPage<PerTicket> page = swarmAggMapper.perTicketPageList(new Page<>(bo.getPageNum(), bo.getPageSize()), bo.isAsc());
+        log.info("按昨日出票进行排序,出参:{}",JSON.toJSONString(page.getRecords()));
+        List<String> list = page.getRecords().stream().map(v -> v.getWalletAddress()).collect(Collectors.toList());
+        log.info("按昨日出票数排序列表:{}",JSON.toJSONString(list));
+        return list;
+    }
+
+    /**
      * 组装昨日有效出票数
      * @param list
      */
@@ -93,20 +115,23 @@ public class SwarmNodeServiceImpl extends ServiceImpl<SwarmNodeMapper, SwarmNode
         param.put("startDate",LocalDate.now().minusDays(1));
         param.put("endDate",LocalDate.now());
         log.info("查询节点每天的出票数，入参:{}", JSON.toJSONString(param));
-        List<Map<String, Object>> perTicketInfos = swarmAggMapper.getPerTicketInfo(param);
+        List<PerTicket> perTicketInfos = swarmAggMapper.getPerTicketInfo(param);
         log.info("查询节点每天的出票数，出参:{}", JSON.toJSONString(perTicketInfos));
-        if(perTicketInfos.size() > 0){
-            Map<String, Long> perTickets = new HashMap<>();
-            perTicketInfos.stream().forEach(v->{
-                String key = (String) v.get("walletAddress");
-                Long value = (Long) v.get("totalPerTicket");
-                perTickets.put(key,value);
-            });
-            list.stream().forEach(v->{
-                Long totalPerTicket = perTickets.get(v.getWalletAddress());
-                v.setYestodayTicketValid(totalPerTicket);
-            });
+        if(perTicketInfos == null || perTicketInfos.size() == 0){
+            return;
         }
+        Map<String, Long> perTickets = new HashMap<>();
+        perTicketInfos.stream().forEach(v->{
+            String key = v.getWalletAddress();
+            Long value = v.getTotalPerTicket();
+            perTickets.put(key,value);
+        });
+        list.stream().forEach(v->{
+            Long totalPerTicket = perTickets.get(v.getWalletAddress());
+            if(totalPerTicket != null){
+                v.setYestodayTicketValid(totalPerTicket);
+            }
+        });
     }
 
 
