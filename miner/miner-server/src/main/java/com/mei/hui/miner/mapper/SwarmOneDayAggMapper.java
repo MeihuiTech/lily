9110bf20fc1eec862017c1a4d85c9764c21a5269
834953332@qq.com <834953332@qq.com>
2021-06-18 17:26:03 +0800

    新增按天聚合表

---
+++ miner/miner-server/src/main/java/com/mei/hui/miner/mapper/SwarmOneDayAggMapper.java
@@ -0,0 +1,9 @@
+package com.mei.hui.miner.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mei.hui.miner.entity.SwarmOneDayAgg;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SwarmOneDayAggMapper extends BaseMapper<SwarmOneDayAgg> {
+}
