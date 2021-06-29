1c06bce6e5ba581fcfcee97b278096ba6c55adf0
834953332@qq.com <834953332@qq.com>
2021-06-29 20:27:40 +0800

    矿机定时器失效问题

--- miner/miner-server/src/main/java/com/mei/hui/miner/common/task/MachineTask.java
+++ miner/miner-server/src/main/java/com/mei/hui/miner/common/task/MachineTask.java
@@ -1,52 +1,57 @@
 package com.mei.hui.miner.common.task;
 
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.mei.hui.miner.entity.SysMachineInfo;
+import com.mei.hui.miner.mapper.SysMachineInfoMapper;
 import com.mei.hui.miner.service.ISysMachineInfoService;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Date;
 import java.util.List;
 
-@Component("machineTask")
+@Configuration
+@EnableScheduling
+@Slf4j
 public class MachineTask {
-    private static final Logger log = LoggerFactory.getLogger(MachineTask.class);
 
     @Autowired
-    private ISysMachineInfoService sysMachineInfoService;
+    private SysMachineInfoMapper sysMachineInfoMapper;
+    @Value("${spring.profiles.active}")
+    private String env;
 
-    /**
-     * 检测矿机是否离线
-     * @param rowCount 每次从数据库获取多少条数据
-     * @param duration 单位秒, 大于这个时间则认为已离线
-     */
-    public void updateMachineOnlineStatus(Integer rowCount, Integer duration) {
-
-        log.info("updateMachineOnlineStatus start...");
-
-        Date now = new Date();
-        int offset = 0;
-        while (true) {
-            List<SysMachineInfo> sysMachineInfoList = sysMachineInfoService.selectSysMachineInfoByLimit(offset, rowCount);
-
-            for (SysMachineInfo sysMachineInfo : sysMachineInfoList) {
-                long date = sysMachineInfo.getUpdateTime().toInstant(ZoneOffset.of("+8")).toEpochMilli();
-                if ((now.getTime() - date) > (duration * 1000)) {
-                    sysMachineInfo.setOnline(0);
-                    int row = sysMachineInfoService.updateSysMachineInfo(sysMachineInfo);
-                    System.out.println(row);
-                }
-            }
-
-            if (sysMachineInfoList.size() < rowCount) {
-                break;
-            }
-
-            offset += rowCount;
+    @Scheduled(cron = "0 */1 * * * ?")
+    public void run() {
+        log.info("======================MachineTask-start===================");
+        if("dev".equals(env)){
+            log.info("开发环境,不执行");
+            return;
         }
-
-        log.info("updateMachineOnlineStatus end...");
+        LambdaQueryWrapper<SysMachineInfo> queryWrapper = new LambdaQueryWrapper();
+        queryWrapper.eq(SysMachineInfo::getOnline,1);
+        List<SysMachineInfo> sysMachineInfoList = sysMachineInfoMapper.selectList(queryWrapper);
+        for (SysMachineInfo sysMachineInfo : sysMachineInfoList) {
+            log.info("处理矿机:{}", JSON.toJSONString(sysMachineInfo));
+            long date = sysMachineInfo.getUpdateTime().toInstant(ZoneOffset.of("+8")).toEpochMilli();
+            long nowDate = LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli();
+            if ((nowDate - date) > (60 * 1000)) {
+                SysMachineInfo machine = new SysMachineInfo();
+                machine.setOnline(0);
+                machine.setId(sysMachineInfo.getId());
+                int row = sysMachineInfoMapper.updateSysMachineInfo(sysMachineInfo);
+                log.info("更新结果:{}",row);
+            }
+        }
+        log.info("======================MachineTask-end===================");
     }
 }
