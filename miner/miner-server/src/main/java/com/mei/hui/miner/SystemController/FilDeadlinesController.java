42dfe9a92432476220431b52f64991fed818878b
shangbin@meihuitech.com <shangbin@meihuitech.com>
2021-06-30 20:21:16 +0800

    普通用户首页WindowPoSt的96个窗口  增加  矿工id  修改为id

--- miner/miner-server/src/main/java/com/mei/hui/miner/SystemController/FilDeadlinesController.java
+++ miner/miner-server/src/main/java/com/mei/hui/miner/SystemController/FilDeadlinesController.java
@@ -28,8 +28,8 @@ public class FilDeadlinesController {
 
     @ApiOperation(value = "普通用户首页WindowPoSt的96个窗口")
     @GetMapping("/ninetySix")
-    public Result selectFilDeadlinesNinetySixList(String minerId){
-        return filDeadlinesService.selectFilDeadlinesNinetySixList(minerId);
+    public Result selectFilDeadlinesNinetySixList(Long id){
+        return filDeadlinesService.selectFilDeadlinesNinetySixList(id);
     }
 
 
