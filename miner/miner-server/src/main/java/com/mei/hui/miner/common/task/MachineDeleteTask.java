8c8276ec1f7036a2d4cee45a8c20067f327a5147
834953332@qq.com <834953332@qq.com>
2021-07-05 13:02:10 +0800

    MP-382 定时删除离线的矿机数据

---
+++ miner/miner-server/src/main/java/com/mei/hui/miner/common/task/MachineDeleteTask.java
@@ -0,0 +1,48 @@
+package com.mei.hui.miner.common.task;
+
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.mei.hui.miner.entity.SysMachineInfo;
+import com.mei.hui.miner.mapper.SysMachineInfoMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableScheduling
+@Slf4j
+public class MachineDeleteTask {
+
+    @Autowired
+    private SysMachineInfoMapper sysMachineInfoMapper;
+    @Value("${spring.profiles.active}")
+    private String env;
+
+    @Scheduled(cron = "0 50 23 */1 * ?")
+    public void run() {
+        log.info("======================MachineDeleteTask-start===================");
+        if("dev".equals(env)){
+            log.info("开发环境,不执行");
+            return;
+        }
+        LambdaQueryWrapper<SysMachineInfo> queryWrapper = new LambdaQueryWrapper();
+        queryWrapper.eq(SysMachineInfo::getOnline,0);
+        queryWrapper.lt(SysMachineInfo::getUpdateTime, LocalDate.now().minusDays(2));
+        List<SysMachineInfo> sysMachineInfoList = sysMachineInfoMapper.selectList(queryWrapper);
+        log.info("数据条数:{}",sysMachineInfoList.size());
+        List<Long> ids = sysMachineInfoList.stream().map(v -> v.getId()).collect(Collectors.toList());
+        if(ids.size() > 0){
+            sysMachineInfoMapper.deleteBatchIds(ids);
+        }
+        log.info("======================MachineDeleteTask-end===================");
+    }
+}
