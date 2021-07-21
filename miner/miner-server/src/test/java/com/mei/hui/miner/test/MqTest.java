66be91b80edd1fc0849b4c5f6896bd9474d883df
834953332@qq.com <834953332@qq.com>
2021-07-21 17:28:16 +0800

    集成rabbitmq

---
+++ miner/miner-server/src/test/java/com/mei/hui/miner/test/MqTest.java
@@ -0,0 +1,29 @@
+package com.mei.hui.miner.test;
+
+import com.mei.hui.miner.MinerApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(classes = MinerApplication.class)
+@RunWith(SpringRunner.class)
+@Slf4j
+public class MqTest {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void sendMsg(){
+        for(int i=0;i<100;i++){
+            //第一个参数为队列名称,第二个参数为要发送的消息对象,这里传的是一个字符串
+            log.info("hello rabbit {}",i);
+            rabbitTemplate.convertAndSend("test","hello rabbit"+i);
+        }
+
+    }
+}
