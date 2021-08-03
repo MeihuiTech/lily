20beecf88bf8a4b442bedd3e7cd0adc825f7e8d5
frrist <forrest@protocol.ai>
2021-08-02 18:00:35 -0700

    more efficent sql for gap finding

--- chain/gap/find.go
+++ chain/gap/find.go
@@ -5,6 +5,7 @@ import (
 	"sort"
 	"time"
 
+	"github.com/deckarep/golang-set"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/sentinel-visor/chain"
@@ -17,6 +18,14 @@ import (
 type GapIndexer struct {
 	DB     *storage.Database
 	opener lens.APIOpener
+}
+
+var TaskSet mapset.Set
+
+func init() {
+	for _, t := range chain.AllTasks {
+		TaskSet.Add(t)
+	}
 }
 
 func NewGapIndexer(o lens.APIOpener, db *storage.Database) *GapIndexer {
@@ -36,6 +45,10 @@ func (g *GapIndexer) Run(ctx context.Context) error {
 	}()
 
 	findLog := log.With("type", "find")
+	missingTasks, err := g.queryTaskGaps(ctx, len(chain.AllTasks))
+	if err != nil {
+		return xerrors.Errorf("detecting missing tasks: %w", err)
+	}
 	heightGaps, err := g.detectProcessingGaps(ctx, node)
 	if err != nil {
 		return xerrors.Errorf("detecting processing gaps: %w", err)
@@ -78,22 +91,23 @@ func (g *GapIndexer) detectSkippedTipSets(ctx context.Context, node lens.API) (v
 	return gapReport, nil
 }
 
-func (g *GapIndexer) detectProcessingGaps(ctx context.Context, node lens.API) (visor.GapReportList, error) {
+func (g *GapIndexer) detectProcessingGaps(ctx context.Context, node lens.API, start, end uint64) (visor.GapReportList, error) {
 	reportTime := time.Now()
-	// TODO unsure how big these lists will be, will likely want some sort of pagination here with limits and a loop over it
 
-	// build list of all reports ordered by height, scan these to find gaps in epochs
-	var reportHeights []visor.ProcessingReport
-	if err := g.DB.AsORM().Model(&reportHeights).
-		Order("height desc").
-		DistinctOn("height").
-		Select(); err != nil {
-		return nil, xerrors.Errorf("query processing report heights: %w", err)
-	}
+	var missingHeights []uint64
+	g.DB.AsORM().QueryContext(
+		ctx,
+		&missingHeights,
+		`
+SELECT s.i AS missing_epoch
+FROM generate_series(?, ?) s(i)
+WHERE NOT EXISTS (SELECT 1 FROM visor_processing_reports WHERE height = s.i);
+`,
+		start, end)
 
-	gapReport := make([]*visor.GapReport, 0, len(reportHeights))
+	gapReport := make([]*visor.GapReport, 0, len(missingHeights))
 	// walk the possible gaps and query lotus to determine if gap was a null round or missed epoch.
-	for _, gap := range findEpochGaps(reportHeights) {
+	for _, gap := range missingHeights {
 		gh := abi.ChainEpoch(gap)
 		tsgap, err := node.ChainGetTipSetByHeight(ctx, gh, types.EmptyTSK)
 		if err != nil {
@@ -115,33 +129,64 @@ func (g *GapIndexer) detectProcessingGaps(ctx context.Context, node lens.API) (v
 	return gapReport, nil
 }
 
-// Example:
-// find the missing heights, that is where there are non-sequential heights:
-// 1
-// 3
-// 5
-// 8
-// missing 2,4,6,7, need to determine if they were missed or null rounds
-func findEpochGaps(prs []visor.ProcessingReport) []int64 {
-	if len(prs) == 0 {
-		return []int64{}
+type TaskHeight struct {
+	Task   string
+	Height uint64
+}
+
+type TasksTODO struct {
+	Tasks  []string
+	Height uint64
+}
+
+// queryTaskGaps returns a list of TasksTODO containing heights and the tasks missing at that height.
+func (g *GapIndexer) queryTaskGaps(ctx context.Context, expectedTasksPerHeight int) ([]TasksTODO, error) {
+	var result []TaskHeight
+	var out []TasksTODO
+	// returns a list of tasks and heights for all incomplete heights
+	// and incomplete height is a height with less than 12 (len.chainAllTasks) entries
+	if _, err := g.DB.AsORM().QueryContext(
+		ctx,
+		&result,
+		`SELECT vpr.task, vpr.height
+				FROM visor_processing_reports vpr
+				LEFT JOIN
+					(
+						SELECT hc.height, hc.cheight
+						FROM (
+								SELECT height, COUNT(height) AS cheight
+								FROM visor_processing_reports
+								GROUP BY height
+							) hc
+				WHERE hc.cheight != ?
+					) incomplete
+				ON vpr.height = incomplete.height`,
+		expectedTasksPerHeight,
+	); err != nil {
+		return nil, err
 	}
-	maybeGap := make([]int64, 0, len(prs))
-	prev := prs[0]
-	for idx := 1; idx < len(prs); idx++ {
-		cur := prs[idx]
-		for gap := cur.Height + 1; gap < prev.Height; gap++ {
-			maybeGap = append(maybeGap, gap)
+
+	// result has all the tasks completed at each height, now we need to find what is missing
+	// at each height.
+	var taskMap = make(map[uint64][]string)
+	for _, th := range result {
+		taskMap[th.Height] = append(taskMap[th.Height], th.Task)
+	}
+
+	for height, tasks := range taskMap {
+		// unsafe means not thread safe, which is fine.
+		querySet := mapset.NewThreadUnsafeSet()
+		for _, t := range tasks {
+			querySet.Add(t)
 		}
-		prev = cur
+		missingTasks := TaskSet.Difference(querySet)
+		for mt := range missingTasks.Iter() {
+			missing := mt.([]string)
+			out = append(out, TasksTODO{
+				Tasks:  missing,
+				Height: height,
+			})
+		}
 	}
-	// check if there are any epochs before the oldest processing report we have
-	for idx := int64(0); idx < prs[len(prs)-1].Height; idx++ {
-		maybeGap = append(maybeGap, idx)
-	}
-	// sort here to optimize the calls to lotus API (caching)
-	sort.Slice(maybeGap, func(i, j int) bool {
-		return maybeGap[i] < maybeGap[j]
-	})
-	return maybeGap
+	return out, nil
 }
