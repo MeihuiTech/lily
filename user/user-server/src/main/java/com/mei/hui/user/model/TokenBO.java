bd15455ee50d086a8287fcd7720c9b850f016ae2
834953332@qq.com <834953332@qq.com>
2021-08-05 10:16:15 +0800

    MP-455 首页收益增长条形图变为多条数据

---
+++ user/user-server/src/main/java/com/mei/hui/user/model/TokenBO.java
@@ -0,0 +1,14 @@
+package com.mei.hui.user.model;
+
+public class TokenBO {
+
+    private Long userId;
+
+    private Long currencyId;
+
+    private String  platform;
+
+    private boolean isVisitor;
+
+
+}
