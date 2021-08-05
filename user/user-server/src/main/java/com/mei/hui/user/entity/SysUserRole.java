0d28ccb353850a396515792c3c68a605d36daff5
834953332@qq.com <834953332@qq.com>
2021-08-05 11:24:37 +0800

    游客登陆去掉管理员登陆选项

--- user/user-server/src/main/java/com/mei/hui/user/entity/SysUserRole.java
+++ user/user-server/src/main/java/com/mei/hui/user/entity/SysUserRole.java
@@ -1,5 +1,6 @@
 package com.mei.hui.user.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Builder;
 import lombok.Data;
@@ -22,7 +23,8 @@ public class SysUserRole
     /** 角色ID */
     private Long roleId;
 
+    @TableField(exist = false)
     private LocalDateTime createTime;
-
+    @TableField(exist = false)
     private LocalDateTime updateTime;
 }
