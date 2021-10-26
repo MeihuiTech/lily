98c20fcc14787aab48d04361cc0b1cf5ce256868
baohj <834953332@qq.com>
2021-10-26 14:10:44 +0800

    引入elasticsearch

---
+++ browser/src/main/java/com/mei/hui/browser/common/es/EsAutoConfiguration.java
@@ -0,0 +1,53 @@
+package com.mei.hui.browser.common.es;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.List;
+
+@Slf4j
+@Configuration
+public class EsAutoConfiguration {
+    @Autowired
+    private EsConfig esConfig;
+    @Bean
+    public RestHighLevelClient client(){
+        List<EsNode> nodes = esConfig.getNodes();
+        if(nodes.size() == 0){
+            log.error("您还没有配置elasticsearch");
+            return null;
+        }
+        HttpHost[] httpHosts = new HttpHost[nodes.size()];
+        for(int i=0;i<nodes.size();i++) {
+            EsNode node= nodes.get(i);
+            httpHosts[i] = new HttpHost(node.getIp(), node.getPort(), node.getSchema());
+        }
+
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(esConfig.getUserName(), esConfig.getPassword()));
+
+        RestClientBuilder builder = RestClient.builder(httpHosts).setRequestConfigCallback(requestConfigBuilder -> {
+            requestConfigBuilder.setConnectTimeout(esConfig.getConnectTimeout());
+            requestConfigBuilder.setSocketTimeout(esConfig.getSocketTimeout());
+            requestConfigBuilder.setConnectionRequestTimeout(esConfig.getConnectionRequestTimeout());
+            return requestConfigBuilder;
+        }).setHttpClientConfigCallback(httpClientBuilder -> {
+            httpClientBuilder.disableAuthCaching();
+            return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+        });
+
+        RestHighLevelClient client = new RestHighLevelClient(builder);
+        return client;
+    }
+
+}
